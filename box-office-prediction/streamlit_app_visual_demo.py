f2cd5848b9fe36be667c93e7fb9c1f38efa18c4e
zongzl999 <94867460+zongzl999@users.noreply.github.com>
2024-03-13 11:06:45 -0700

    Add files via upload
    Added graphs, updated dataset

--- box-office-prediction/streamlit_app_visual_demo.py
+++ box-office-prediction/streamlit_app_visual_demo.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import os
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_directory, 'data_clean_v2.csv')
+file_path = os.path.join(script_directory, '../data/cleaned/data_clean_v6.csv')
 film_data = pd.read_csv(file_path)
 
 st.header('Popcorn Pulse', divider='red')
@@ -17,6 +19,8 @@ tab6, tab7, tab8, tab9, tab10 = st.tabs(["Average Rating by Genres","Way of pres
                                           "Runtime vs. Average Rating", "Average Revenue by Actor", "Average Revenue by Actress"])
 tab11, tab12, tab13, tab14 = st.tabs(["Average Revenue by Director", "Average Rating by Director",
                                       "Number of Films by Genre", "Distribution of Film Runtime"])
+tab15, tab16, tab17, tab18 = st.tabs(["Box Plot: Rating by Genre", "Pair Plot for Avg Rating, Runtime and Revenue",
+                                      "Correlation Matrix for Avg Rating, Runtime and Revenue", "3D Scatter Plot: Runtime, Rating, and Revenue"])
 
 with tab1:
    st.header('Top Rows of the Dataset')
@@ -33,8 +37,8 @@ with tab2:
 
 with tab3:
    st.header('Revenue by Genre')
-   film_data['Revenue'] = pd.to_numeric(film_data['Revenue'].replace('[\$,]', '', regex=True), errors='coerce')
-   revenue_by_genre = film_data.groupby('genres')['Revenue'].sum().sort_values(ascending=False)
+   film_data['Revenue_InflationCorrected'] = pd.to_numeric(film_data['Revenue_InflationCorrected'])
+   revenue_by_genre = film_data.groupby('genres')['Revenue_InflationCorrected'].sum().sort_values(ascending=False)
 
    fig, ax = plt.subplots()
    revenue_by_genre.plot(kind='bar', ax=ax, color='salmon')
@@ -44,14 +48,14 @@ with tab3:
 
 with tab4:
    fig, ax = plt.subplots()
-   ax.scatter(film_data['averageRating'], film_data['Revenue'], alpha=0.5)
+   ax.scatter(film_data['averageRating'], film_data['Revenue_InflationCorrected'], alpha=0.5)
    ax.set_xlabel('Average Rating')
    ax.set_ylabel('Revenue')
    ax.set_title('Revenue vs. Average Rating')
    st.pyplot(fig)
 
 with tab5:
-   average_revenue_by_genre = film_data.groupby('genres')['Revenue'].mean().sort_values(ascending=False)
+   average_revenue_by_genre = film_data.groupby('genres')['Revenue_InflationCorrected'].mean().sort_values(ascending=False)
    fig, ax = plt.subplots()
    average_revenue_by_genre.plot(kind='bar', color='lightgreen', ax=ax)
    ax.set_xlabel('Genre')
@@ -88,7 +92,7 @@ with tab8:
    st.pyplot(fig)
 
 with tab9:
-   revenue_by_actor = film_data.groupby('actor')['Revenue'].mean().sort_values(ascending=False).head(10) # Limiting to top 10 for readability
+   revenue_by_actor = film_data.groupby('actor')['Revenue_InflationCorrected'].mean().sort_values(ascending=False).head(10)
    fig, ax = plt.subplots()
    revenue_by_actor.plot(kind='bar', color='coral', ax=ax)
    ax.set_xlabel('Actor')
@@ -96,7 +100,7 @@ with tab9:
    st.pyplot(fig)
 
 with tab10:
-   revenue_by_actress = film_data.groupby('actress')['Revenue'].mean().sort_values(ascending=False).head(10) # Limiting to top 10 for readability
+   revenue_by_actress = film_data.groupby('actress')['Revenue_InflationCorrected'].mean().sort_values(ascending=False).head(10)
    fig, ax = plt.subplots()
    revenue_by_actress.plot(kind='bar', color='coral', ax=ax)
    ax.set_xlabel('Actress')
@@ -104,7 +108,7 @@ with tab10:
    st.pyplot(fig)
 
 with tab11:
-   revenue_by_director = film_data.groupby('director')['Revenue'].mean().sort_values(ascending=False).head(10) # Limiting to top 10 for readability
+   revenue_by_director = film_data.groupby('director')['Revenue_InflationCorrected'].mean().sort_values(ascending=False).head(10)
    fig, ax = plt.subplots()
    revenue_by_director.plot(kind='bar', color='coral', ax=ax)
    ax.set_xlabel('Director')
@@ -112,7 +116,7 @@ with tab11:
    st.pyplot(fig)
 
 with tab12:
-   average_rating_by_director = film_data.groupby('director')['averageRating'].mean().sort_values(ascending=False).head(10)  # Limiting to top 10 for readability
+   average_rating_by_director = film_data.groupby('director')['averageRating'].mean().sort_values(ascending=False).head(10)
    fig, ax = plt.subplots()
    average_rating_by_director.plot(kind='bar', color='skyblue', edgecolor='black')
    ax.set_xlabel('Director')
@@ -134,4 +138,45 @@ with tab14:
    ax.set_xlabel('Runtime (minutes)')
    ax.set_ylabel('Frequency')
    ax.set_title('Distribution of Film Runtimes')
+   st.pyplot(fig)
+
+with tab15:
+   fig, ax = plt.subplots(figsize=(12, 8))
+   sns.boxplot(x='genres', y='averageRating', data=film_data, ax=ax)
+   ax.set_xticks(ax.get_xticks())
+   ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+   ax.set_title('Box Plot: Rating by Genre')
+   st.pyplot(fig)
+
+with tab16:
+   selected_variables = ['averageRating', 'runtimeMinutes', 'Revenue_InflationCorrected']
+   pair_plot_data = film_data[selected_variables]
+   pair_plot_data = pair_plot_data.rename(columns={'Revenue_InflationCorrected': 'Revenue'})
+   pairplot = sns.pairplot(pair_plot_data)
+   st.pyplot(pairplot.fig)
+
+with tab17:
+   selected_variables = ['averageRating', 'runtimeMinutes', 'Revenue_InflationCorrected']
+   correlation_matrix_data = film_data[selected_variables]
+   correlation_matrix_data = correlation_matrix_data.rename(columns={'Revenue_InflationCorrected': 'Revenue'})
+   correlation_matrix = correlation_matrix_data.corr()
+   fig, ax = plt.subplots()
+   sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
+   ax.set_title('Correlation Matrix for Avg Rating, Runtime and Revenue')
+   st.pyplot(fig)
+
+with tab18:
+   selected_variables = ['runtimeMinutes', 'averageRating', 'Revenue_InflationCorrected']
+   scatter_data = film_data[selected_variables]
+   scatter_data = scatter_data.rename(columns={'Revenue_InflationCorrected': 'Revenue'})
+
+   fig = plt.figure(figsize=(10, 8))
+   ax = fig.add_subplot(111, projection='3d')
+   ax.scatter(scatter_data['runtimeMinutes'], scatter_data['averageRating'], scatter_data['Revenue'], c='coral', marker='o', alpha=0.5)
+   ax.set_xlabel('Runtime (minutes)')
+   ax.set_ylabel('Average Rating')
+   ax.set_zlabel('Revenue')
+   ax.view_init(elev=20, azim=-45)
+   ax.set_title('3D Scatter Plot: Runtime, Rating, and Revenue')
+   ax.zaxis.labelpad = -2
    st.pyplot(fig)
