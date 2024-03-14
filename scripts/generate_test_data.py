8e04eaf38323ae8c5036ef1a1308c1d45fb058ee
= <=>
2024-03-13 22:07:12 -0700

    updated directory structure

---
+++ scripts/generate_test_data.py
@@ -0,0 +1,29 @@
+"""
+Module to generate test data from a CSV file.
+"""
+
+import os
+import random
+import pandas as pd
+
+def generate_data(filepath):
+    """
+    Generate test data from a CSV file.
+
+    Args:
+        filepath (str): Path to the CSV file.
+
+    Returns:
+        None
+    """
+    absolute_path = os.path.abspath(filepath)
+
+    df = pd.read_csv(absolute_path)
+    test_df = df.copy()
+
+    for col in test_df.columns:
+        test_df[col] = random.sample(test_df[col].tolist(), len(test_df[col]))
+    test_df.head(100).to_csv("test_data_movies.csv", index=False)
+
+FILE_PATH = "../../../data/cleaned/data_clean_v6.csv"
+generate_data(FILE_PATH)
